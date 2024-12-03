50b9603b3f2073e0e5b1ff1362051b04f168d54f
BobbyMoreau <bobbymoreauraquin@gmail.com>
2024-12-03 12:56:35 +0100

    day 3 part 1

---
+++ day03/Program.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Text.RegularExpressions;
+
+public class Day03
+{
+    static void Main(string[] args)
+    {
+
+        var part = args.Length > 0 ? args[0].ToLower() : "part1";
+        var fileName = ("input.txt");
+        string file = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+        var solution = part switch
+        {
+            "part1" => Part1(file),
+            "part2" => Part2(file),
+            _ => throw new ArgumentOutOfRangeException(nameof(part), $"Unexpected {nameof(part)} value: '{part}'")
+        };
+
+        Console.WriteLine($"Solution for {part}:");
+        Console.WriteLine(solution);
+    }
+
+    static int Part1(string file)
+    {
+        string pattern = @"mul\((\d{1,3}),(\d{1,3})\)";
+        int sum = 0;    
+
+        Regex regex = new Regex(pattern);
+        string content = File.ReadAllText(file);
+        MatchCollection matches = regex.Matches(content);
+
+        foreach (Match match in matches)
+        {
+
+                string numberOne = match.Groups[1].Value;
+                string numberTwo = match.Groups[2].Value;
+
+                int factorOne = int.Parse(numberOne);
+                int factorTwo = int.Parse(numberTwo);
+
+                sum += (factorOne * factorTwo);
+
+        }
+
+        return sum;
+    }
+
+    static int Part2(string file)
+    {
+        return 0;
+    }
+}
