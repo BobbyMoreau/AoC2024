9b106d9bbe61868083d01d9bac7e1757270f1cc2
BobbyMoreau <bobbymoreauraquin@gmail.com>
2024-12-05 11:26:28 +0100

    day 4 not started

---
+++ day04/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Day04
+{
+    static void Main(string[] args)
+    {
+
+        var part = args.Length > 0 ? args[0].ToLower() : "part1";
+        var fileName =("input.txt");
+        string file = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+        var solution = part switch
+        {
+            "part1" => Part1(file),
+            "part2" => Part2(file),
+            _ => throw new ArgumentOutOfRangeException(nameof(part), $"Unexpected {nameof(part)} value: '{part}'")
+        };
+
+        Console.WriteLine($"Solution for {part}:");
+        Console.WriteLine(solution);
+    }
+
+    static int Part1(string file)
+    {
+        
+    }
+
+    static int Part2(string file)
+    {
+        
+    }
+
+
+    
+}
